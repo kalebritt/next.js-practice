e74fc30d2db97e5ceeea93bfd19143238aa99ae1
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-03-25 15:42:51 -0600

    some fun

--- jspractice/if-else.js
+++ jspractice/if-else.js
@@ -5,3 +5,20 @@ if (sale) {
 } else {
   console.log("Time to sell!");
 }
+
+//when hunger strikes
+let hungerLevel = 12;
+if (hungerLevel > 7) {
+  console.log("Time to eat!");
+} else {
+  console.log("We can eat later!");
+}
+
+//when sleep strikes
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if(mood === 'sleepy' && tirednessLevel > 8) {
+  console.log('time to sleep')
+} else {
+  console.log('not bed time yet')
+}
