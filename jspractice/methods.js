670844e654730db115c3dea1c497df9557e09b4f
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-03-25 09:39:10 -0600

    added comments

--- jspractice/methods.js
+++ jspractice/methods.js
@@ -1,4 +1,8 @@
+//Math.random() is a method that generates a random number between 0 and 1
 console.log(Math.random() * 100);
+//Math.floor() is a method that rounds a number to the nearest integer
 console.log(Math.floor(Math.random() * 100));
+//Math.ceil() is a method that rounds a number to the nearest integer between 0 and 1
 console.log(Math.ceil(43.8));
+//Number.isInteger() is a method that checks if a number is an integer between 0 and 1
 console.log(Number.isInteger(2017));
