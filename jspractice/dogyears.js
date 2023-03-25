083cd1ab8f35089eb4e3095ef875c3de525af14d
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-03-25 15:08:16 -0600

    added dogyears.js and code

---
+++ jspractice/dogyears.js
@@ -0,0 +1,15 @@
+//this is how old i am
+const myAge = 2000;
+//some comments explaining the next line
+let earlyYears = 2;
+earlyYears *= 10.5;
+//more comments
+let laterYears = myAge - 2;
+//comments
+laterYears *= 4;
+console.log(earlyYears + " and " + laterYears);
+//comment
+const myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+const myName = 'keeb'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
