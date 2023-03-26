6df3dc4a7738b5cfe0dbddc90a59617c84244f5b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-03-26 12:26:58 -0600

    fixed else-if statement

--- jspractice/eightBall.js
+++ jspractice/eightBall.js
@@ -3,22 +3,51 @@ userName ? console.log(`Hello, ${userName}.`) : console.log("Hello.");
 const userQuestion = "Ask something, ";
 console.log(userQuestion + userName);
 const randomNumber = Math.floor(Math.random() * 8);
-const eightBall = "";
+let eightBall = "";
+console.log(randomNumber);
+
 if (randomNumber === 0) {
-  eightBall === "It is certain.";
+  eightBall = "It is certain.";
 } else if (randomNumber === 1) {
-  eightBall === "It is decidedly so.";
+  eightBall = "It is decidedly so.";
 } else if (randomNumber === 2) {
-  eightBall === "Reply hazy, try again.";
+  eightBall = "Reply hazy, try again.";
 } else if (randomNumber === 3) {
-  eightBall === "Cannot predict future now.";
+  eightBall = "Cannot predict future now.";
 } else if (randomNumber === 4) {
-  eightBall === "Do not count on it.";
+  eightBall = "Do not count on it.";
 } else if (randomNumber === 5) {
-  eightBall === "My sources say no.";
+  eightBall = "My sources say no.";
 } else if (randomNumber === 6) {
-  eightBall === "Outlook not so good";
-} else {
-  eightBall === "Signs point to yes.";
+  eightBall = "Outlook not so good";
+} else if (randomNumber === 7) {
+  eightBall = "Signs point to yes.";
 }
+
+// switch (randomNumber) {
+//   case 0:
+//     console.log("It is certain.");
+//     break;
+//   case 1:
+//     console.log("It is decidedly so.");
+//     break;
+//   case 2:
+//     console.log("Reply hazy, try again.");
+//     break;
+//   case 3:
+//     console.log("Cannot predict future now.");
+//     break;
+//   case 4:
+//     console.log("Do not count on it.");
+//     break;
+//   case 5:
+//     console.log("My sources say no.");
+//     break;
+//   case 6:
+//     console.log("Outlook not so good");
+//     break;
+//   case 7:
+//     console.log("Signs point to yes.");
+//     break;
+// }
 console.log(eightBall);
