f4ce421e9f5ff6a2200d5db95e81c5b9c5611237
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-03-26 08:14:50 -0600

    added comments

--- jspractice/case-break.js
+++ jspractice/case-break.js
@@ -1,5 +1,5 @@
 let athleteFinalPosition = 'first place';
-
+//switch statements take in two parameters, the first one is the switch statement and the second one is the value to which the switch statement is assigned.
 switch(athleteFinalPosition){
   case 'first place':
     console.log('You get the gold medal!');
