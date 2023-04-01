6840c1d617b5fe0fe4a0ba1ab850288b80e5a869
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-01 08:18:37 -0600

    changed html file and fixed typos

--- jspractice/race.js
+++ jspractice/race.js
@@ -2,12 +2,12 @@ let raceNumber = Math.floor(Math.random() * 1000);
 
 let registeredEarly = true;
 
-let age = 0;
+let age = 18;
 
 if (age >= 18 && registeredEarly) {
   raceNumber += 1000;
 }
 
 if(age >= 18 && registeredEarly) {
-  console.log(`You will race at 9:30 am and your race numver is ${raceNumber}.`)
+  console.log(`You will race at 9:30 am and your race number is ${raceNumber}.`)
 }
