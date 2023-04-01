26bb23b916a91a0569c63d8ff0ee155f02253bb0
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-01 12:49:09 -0600

    more practice

--- jspractice/functions.js
+++ jspractice/functions.js
@@ -10,8 +10,17 @@ sayThanks("Cole");
 
 //return statement
 function monitorCount(rows, columns) {
-    return rows * columns
-  }
-  
-  const numOfMonitors = monitorCount(5,4)
-  console.log(numOfMonitors)
+  return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+console.log(numOfMonitors);
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200;
+}
+const totalCost = costOfMonitors(5, 4);
+console.log(totalCost);
