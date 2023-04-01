767c60f67865389e05134a24b179cf8af9bda882
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-01 09:00:27 -0600

    some code

--- jspractice/functions.js
+++ jspractice/functions.js
@@ -1,1 +1,9 @@
-// When declaring a function, we can specify its parameters. Parameters allow functions to accept input(s) and perform a task using the input(s). We use parameters as placeholders for information that will be passed to the function when it is called.
+// When declaring a function, we can specify its parameters. Parameters allow functions to accept input(s) and perform a task using the input(s). We use parameters as placeholders for information that will be passed to the function when it is called. Parameters are treated like variables within a function.
+
+function sayThanks(name) {
+  console.log(
+    "Thank you for your purchase " + name + "! We appreciate your business."
+  );
+}
+
+sayThanks("Cole");
