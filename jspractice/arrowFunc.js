a775f2cd1aa23c78b9f11dc128d93bd7c807fe5f
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-02 08:34:35 -0600

    arrow functions

---
+++ jspractice/arrowFunc.js
@@ -0,0 +1,5 @@
+// Arrow functions remove the need to type out the keyword function every time you need to create a function. Instead, you first include the parameters inside the ( ) and then add an arrow => that points to the function body surrounded in { } like this:
+const rectangleArea = (width, height) => {
+    let area = width * height;
+    return area;
+  };
