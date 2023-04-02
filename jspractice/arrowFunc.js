e336cd07033003eabfaf67a00ec8b41506d5e9da
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-02 09:31:28 -0600

    arrow function fun

--- jspractice/arrowFunc.js
+++ jspractice/arrowFunc.js
@@ -1,5 +1,18 @@
 // Arrow functions remove the need to type out the keyword function every time you need to create a function. Instead, you first include the parameters inside the ( ) and then add an arrow => that points to the function body surrounded in { } like this:
 const rectangleArea = (width, height) => {
-    let area = width * height;
-    return area;
-  };
+  let area = width * height;
+  return area;
+};
+
+//   Functions that take only a single parameter do not need that parameter to be enclosed in parentheses. However, if a function takes zero or multiple parameters, parentheses are required.
+
+//zero parameters
+const functionName = () => {};
+
+//one parameter
+const functionNameOne = (paraOne) => {};
+
+//two or more parameters
+const functionNameTwo = (paraOne, paraTwo) => {};
+
+// A function body composed of a single-line block does not need curly braces. Without the curly braces, whatever that line evaluates will be automatically returned. The contents of the block should immediately follow the arrow => and the return keyword can be removed. This is referred to as implicit return.
