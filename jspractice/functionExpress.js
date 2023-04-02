3a46359c4a9d5c1fd1512afc3e7634719fd70ff4
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-02 08:30:27 -0600

    some code

--- jspractice/functionExpress.js
+++ jspractice/functionExpress.js
@@ -1,1 +1,10 @@
 // Another way to define a function is to use a function expression. To define a function inside an expression, we can use the function keyword. In a function expression, the function name is usually omitted. A function with no name is called an anonymous function. A function expression is often stored in a variable in order to refer to it.
+const plantNeedsWater = function (day) {
+  if (day === "Wednesday") {
+    return true;
+  } else {
+    return false;
+  }
+};
+plantNeedsWater("Tuesday");
+console.log(plantNeedsWater("Tuesday"));
