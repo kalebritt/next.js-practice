5e7d5187c0833b274810e1509f8cf375c39cf429
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-03 16:03:02 -0600

    some comments and examples

--- jspractice/arrowFunc.js
+++ jspractice/arrowFunc.js
@@ -16,3 +16,12 @@ const functionNameOne = (paraOne) => {};
 const functionNameTwo = (paraOne, paraTwo) => {};
 
 // A function body composed of a single-line block does not need curly braces. Without the curly braces, whatever that line evaluates will be automatically returned. The contents of the block should immediately follow the arrow => and the return keyword can be removed. This is referred to as implicit return.
+
+//single line block
+const sumNumbers = (number) => +number;
+
+//multi-line block
+const sumNumbersTwo = (number) => {
+  const sum = number + number;
+  return sum; //return statement
+};
