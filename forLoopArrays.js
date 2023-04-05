20facb526f37542e1daaa58833465901eb27fe6e
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-05 09:02:03 -0600

    array looping

--- forLoopArrays.js
+++ forLoopArrays.js
@@ -12,3 +12,8 @@ for (let i = 0; i < animals.length; i++) {
 // Sea Lion
 
 // In the loop above, we’ve named our iterator variable i. This is a variable naming convention you’ll see in a lot of loops. When we use i to iterate through arrays we can think of it as being short-hand for the word index. Notice how our stopping condition checks that i is less than animals.length. Remember that arrays are zero-indexed, the index of the last element of an array is equivalent to the length of that array minus 1. If we tried to access an element at the index of animals.length we will have gone too far!
+const vacationSpots = ["Bali", "Paris", "Tulum"];
+for (let i = 0; i < vacationSpots.length; i++) {
+    console.log(`I would like to visit ${vacationSpots[i]}`);
+  }
+  
