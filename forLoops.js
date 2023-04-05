3f2842d66f724bb4255bf72f8216dc306b580b0b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-05 08:21:13 -0600

    comments on for loops in forLoops.js

---
+++ forLoops.js
@@ -0,0 +1,3 @@
+// Instead of writing out the same code over and over, loops allow us to tell computers to repeat a given block of code on its own. One way to give computers these instructions is with a for loop.
+
+// The typical for loop includes an iterator variable that usually appears in all three expressions. The iterator variable is initialized, checked against the stopping condition, and assigned a new value on each loop iteration. Iterator variables can have any name, but it’s best practice to use a descriptive variable name.
