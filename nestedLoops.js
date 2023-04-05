6825701221b38fa7122f773cc4da972b02195803
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-05 11:05:30 -0600

    nestedLoops.js, comments, and code snippet

---
+++ nestedLoops.js
@@ -0,0 +1,10 @@
+const myArray = [6, 19, 20];
+const yourArray = [19, 81, 2];
+for (let i = 0; i < myArray.length; i++) {
+  for (let j = 0; j < yourArray.length; j++) {
+    if (myArray[i] === yourArray[j]) {
+      console.log("Both arrays have the number: " + yourArray[j]);
+    }
+  }
+}
+// Let’s think about what’s happening in the nested loop in our example. For each element in the outer loop array, myArray, the inner loop will run in its entirety comparing the current element from the outer array, myArray[i], to each element in the inner array, yourArray[j]. When it finds a match, it prints a string to the console.
