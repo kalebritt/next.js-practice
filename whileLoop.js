4f9e3de03380faeade5fa07bf8d26fd1fc700f3b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2023-04-06 11:18:06 -0600

    more examples

--- whileLoop.js
+++ whileLoop.js
@@ -17,3 +17,11 @@ while (counterTwo < 4) {
 //   What would happen if we didn’t increment counterTwo inside our block? If we didn’t include this, counterTwo would always have its initial value, 1. That would mean the testing condition counterTwo < 4 would always evaluate to true and our loop would never stop running! Remember, this is called an infinite loop and it’s something we always want to avoid. Infinite loops can take up all of your computer’s processing power potentially freezing your computer.
 
 //   So you may be wondering when to use a while loop! The syntax of a while loop is ideal when we don’t know in advance how many times the loop should run. Think of eating like a while loop: when you start taking bites, you don’t know the exact number you’ll need to become full. Rather you’ll eat while you’re hungry. In situations when we want a loop to execute an undetermined number of times, while loops are the best choice.
+
+// Create a while loop with a condition that checks if the currentCard does not have that value 'spade'.
+
+// Inside the block of your while loop, add the following line of code:
+while (currentCard != "spade") {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+}
+
